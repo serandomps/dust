6c146baa07d2926b4d816796ce4eb25471a1f13e
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-09-10 09:20:16 +0530

    updating

--- filters.js
+++ filters.js
@@ -3,5 +3,8 @@ var utils = require('utils');
 module.exports = {
     url: function (value) {
         return utils.resolve(value);
+    },
+    year: function (value) {
+        return moment(value).year();
     }
 };
