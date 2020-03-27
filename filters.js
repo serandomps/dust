b207f46b2cebc84c93c1d8835d720b0a633ae2a9
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-03-27 08:52:02 +0530

    updating

--- filters.js
+++ filters.js
@@ -7,6 +7,9 @@ module.exports = {
     year: function (value) {
         return moment(value).year();
     },
+    numeral: function (value) {
+        return numeral(value).format('(0,0)');
+    },
     ago: function (value) {
         return moment(value).fromNow();
     }
